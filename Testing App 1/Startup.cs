7af3e66fcba101375732c0c009382504c11af9fc
Kamil <kamil600@gmail.com>
2021-04-08 22:27:17 +0200

    Testing typo

--- Testing App 1/Startup.cs
+++ Testing App 1/Startup.cs
@@ -26,7 +26,7 @@ namespace Testing_App_1
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. and st...
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
